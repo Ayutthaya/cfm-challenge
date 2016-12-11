e02453e3e29601c570caa90b3c73dfd239ff2adc
Nathanaël Foy <nathanael.foy@gmail.com>
2016-12-11 11:24:15 +0100

    tempo

--- python/train-xgboost.py
+++ python/train-xgboost.py
@@ -6,9 +6,11 @@ import xgboost as xgb
 import gc
 
 
-TRAINFILE='/home/ubuntu/data/training_input.csv'
-TESTFILE='/home/ubuntu/data/testing_input.csv'
-LABELFILE='/home/ubuntu/data/challenge_output_data_training_file_prediction_of_trading_activity_within_the_order_book.csv'
+#DATADIR='/home/ubuntu/data/'
+DATADIR='/home/nath/Projects/challenge-cfm/data/'
+TRAINFILE=DATADIR+'training_input.csv'
+TESTFILE=DATADIR+'testing_input.csv'
+LABELFILE=DATADIR+'challenge_output_data_training_file_prediction_of_trading_activity_within_the_order_book.csv'
 
 TRAINPICKLE='train.pkl'
 TESTPICKLE='test.pkl'
@@ -33,67 +35,44 @@ SKIPIDANDOFFSET=2
 SKIPFEATURES = 5
 NEWFEATURES = ['spread', 'bid_plus_ask', 'bid_pct']
 
+COMPRESSION=True
+
 for stage in ('train', 'test'):
 
     print('loading data')
     if stage=='train':
-        data_all = pandas.read_csv(TRAINFILE, usecols=USEFEATURES)
+        data_all = pandas.read_csv(TRAINFILE, usecols=USEFEATURES, nrows=NCHUNKS*CHUNKSIZE)
     else:
-        data_all = pandas.read_csv(TESTFILE, usecols=USEFEATURES)
+        data_all = pandas.read_csv(TESTFILE, usecols=USEFEATURES, nrows=NCHUNKS*CHUNKSIZE)
 
     print('creating new features')
     data_all['spread'] = data_all['ask_1']-data_all['bid_1']
     data_all['bid_plus_ask'] = data_all['bid_size_1']-data_all['ask_size_1']
     data_all['bid_pct'] = data_all['bid_size_1']/data_all['bid_plus_ask']
 
-    print('computing diff')
-    data_diff = data_all.iloc[:, SKIPFEATURES:].diff()
+    if COMPRESSION:
+        print('computing diff using compression')
+        data_diff = data_all.ix[(data_all['offset']<=-500) & (data_all['offset']==0), SKIPFEATURES:].diff()
+        n_samples_per_epoch = 3
 
-    print('compressing dataset')
-    data_compressed = data_all[data_all['offset']<=-500 & data_all['offset']==0]
+    else:
+        print('computing diff')
+        data_diff = data_all.iloc[:, SKIPFEATURES:].diff()
+        n_samples_per_epoch = 8
 
-    print('computing compressed diff')
-    data_diff = data_compressed.iloc[:, SKIPFEATURES:].diff()
+    print('computing rolling mean')
+    data_roll_mean = np.hstack([data_all.iloc[7::8, SKIPFEATURES].rolling(window=window, center=True).mean() for window in [MINUTE, TENMINUTES, HOUR, DAY]])
 
-    print('computing rolling mean 1 min')
-    data_roll_mean_1m = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=MINUTE, center=True).mean()
-
-    print('computing rolling mean 10 min')
-    data_roll_mean_10m = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=TENMINUTES, center=True).mean()
-
-    print('computing rolling mean 1 hour')
-    data_roll_mean_1h = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=HOUR, center=True).mean()
-
-    print('computing rolling mean 1 day')
-    data_roll_mean_1d = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=DAY, center=True).mean()
-
-    print('computing rolling std 1 min')
-    data_roll_std_1m = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=MINUTE, center=True).std()
-
-    print('computing rolling std 10 min')
-    data_roll_std_10m = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=TENMINUTES, center=True).std()
-
-    print('computing rolling std 1 hour')
-    data_roll_std_1h = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=HOUR, center=True).std()
-
-    print('computing rolling std 1 day')
-    data_roll_std_1d = data_all.iloc[7::8, SKIPFEATURES:].rolling(window=DAY, center=True).std()
+    print('computing rolling std')
+    data_roll_std = np.hstack([data_all.iloc[7::8, SKIPFEATURES].rolling(window=window, center=True).std() for window in [MINUTE, TENMINUTES, HOUR, DAY]])
 
     print('computing past diff')
-    past_diff_feat = np.hstack([data_diff.iloc[i::8, :].values for i in range(1,8)])
+    past_diff_feat = np.hstack([data_diff.iloc[i::n_samples_per_epoch, :].values for i in range(1,n_samples_per_epoch)])
 
     print('computing fut diff')
-    fut_diff_feat = np.hstack([data_diff.iloc[i+7::8, :].values for i in range(1,9)])
+    fut_diff_feat= np.hstack([data_diff.iloc[i+n_samples_per_epoch-1::n_samples_per_epoch, :].values for i in range(1,n_samples_per_epoch+1)])
     # fix end
     fut_diff_feat = np.r_[fut_diff_feat, np.zeros((1, fut_diff_feat.shape[1]))]
-
-    print('computing past diff compressed')
-    past_diff_feat_compressed = np.hstack([data_diff.iloc[i::3, :].values for i in range(1,3)])
-
-    print('computing fut diff compressed')
-    fut_diff_feat_compressed = np.hstack([data_diff.iloc[i+2::3, :].values for i in range(1,4)])
-    # fix end
-    fut_diff_feat_compressed = np.r_[fut_diff_feat_compressed, np.zeros((1, fut_diff_feat_compressed.shape[1]))]
 
     print('computing pres orig')
     pres_orig_feat = data_all.iloc[7::8, SKIPFEATURES:].values
@@ -111,7 +90,9 @@ for stage in ('train', 'test'):
     entropy_feat = data_all.ix[7::8, ['bid_entropy_1', 'ask_entropy_1']]
 
     print('create train/testset')
-    data=np.c_[past_diff_feat_compressed, fut_diff_feat_compressed, pres_orig_feat, time_feat, data_roll_mean_1m, data_roll_mean_10m, data_roll_mean_1h, data_roll_mean_1d, data_roll_std_1m, data_roll_std_10m, data_roll_std_1h, data_roll_std_1d]
+    for data in [past_diff_feat, fut_diff_feat, pres_orig_feat, time_feat, data_roll_mean, data_roll_std]:
+        print(data.shape)
+    data=np.c_[past_diff_feat, fut_diff_feat, pres_orig_feat, time_feat, data_roll_mean, data_roll_std]
 
     print('data shape: %s' % repr(data.shape))
 
