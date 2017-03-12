c058fd960228d7aede248628dbf0a7101ea2ab90
Nathanaël Foy <nathanael.foy@gmail.com>
2017-03-12 01:11:53 +0100

    implement new features

--- python/feature-engineering.py
+++ python/feature-engineering.py
@@ -41,15 +41,13 @@ for stage in ('train', 'test'):
 
     print('creating new features')
     data_all['spread'] = data_all['ask_1']-data_all['bid_1']
-    data_all['bid_plus_ask'] = data_all['bid_size_1']-data_all['ask_size_1']
+    data_all['bid_plus_ask'] = data_all['bid_size_1']+data_all['ask_size_1']
     data_all['bid_pct'] = data_all['bid_size_1']/data_all['bid_plus_ask']
-
     data_all['eff_bid'] = data_all['bid_1']*data_all['bid_size_1'] + (data_all['bid_1'] - 1)*data_all['bid_size_2']
     data_all['eff_bid'] /= data_all['bid_size_1'] + data_all['bid_size_2']
     data_all['eff_ask'] = data_all['ask_1']*data_all['ask_size_1'] + (data_all['ask_1'] + 1)*data_all['ask_size_2']
     data_all['eff_ask'] /= data_all['ask_size_1'] + data_all['ask_size_2']
     data_all['emid'] = (data_all['eff_bid'] + data_all['eff_ask'])/2
-
     data_all['omid'] = data_all['bid_1']*(1-data_all['bid_pct']) + data_all['ask_1']*data_all['bid_pct']
 
     if COMPRESSION:
@@ -71,6 +69,9 @@ for stage in ('train', 'test'):
     print('computing rolling std')
     data_roll_std = np.hstack([data_all.iloc[7::8, SKIPFEATURES:].rolling(window=window, center=True).std() for window in [MINUTE, TENMINUTES, HOUR, DAY]])
 
+    print('computing local mean')
+    data_local_trend = data_all.iloc[:, SKIPFEATURES:].rolling(window=8).mean()[7::8]
+
     print('computing local trend')
     data_local_trend = data_all.iloc[:, SKIPFEATURES:].rolling(window=8).mean()[7::8] - data_all[7::8]
 
@@ -81,7 +82,7 @@ for stage in ('train', 'test'):
     past_diff_feat = np.hstack([data_diff.iloc[i::n_samples_per_epoch, :].values for i in range(1,n_samples_per_epoch)])
 
     print('computing fut diff')
-    fut_diff_feat= np.hstack([data_diff.iloc[i+n_samples_per_epoch-1::n_samples_per_epoch, :].values for i in range(1,n_samples_per_epoch+1)])
+    fut_diff_feat= data_diff.iloc[n_samples_per_epoch::n_samples_per_epoch, :]
     # fix end
     fut_diff_feat = np.r_[fut_diff_feat, np.zeros((1, fut_diff_feat.shape[1]))]
 
@@ -91,19 +92,20 @@ for stage in ('train', 'test'):
     print('computing past orig')
     past_orig_feat = np.hstack([data_all.iloc[i::8, SKIPIDANDOFFSET:len(USEFEATURES)].values for i in range(8)])
 
-    print('computing time feature')
-    if stage=='train':
-        time_feat = (data_all.iloc[7::8, 0].values-1)%DAY
-    else:
-        time_feat = (data_all.iloc[7::8, 0].values-1+HALFDAY)%DAY
+    print("computing deltas")
+    data_deltas = np.hstack([data_all.iloc[7::8] - data_all[7::8].shift(delta) for delta in range(-5, -1, 1, 5)])
+    data_local_mean_deltas = np.hstack([data_local_mean - data_local_mean.shift(delta) for delta in range(-5, -1, 1, 5)])
 
-    print('computing entropy-based features')
-    entropy_feat = data_all.ix[7::8, ['bid_entropy_1', 'ask_entropy_1']]
+    print('computing time feature')
+    time_feat_halfday = (data_all.iloc[7::8, 0].values-1)%HALFDAY
+    if stage=='train':
+        time_feat_day = (data_all.iloc[7::8, 0].values-1)%DAY
+    else:
+        time_feat_day = (data_all.iloc[7::8, 0].values-1+HALFDAY)%DAY
+    time_feat = np.hstack([time_feat_halfday, time_feat_day])
 
     print('create train/testset')
-    for data in [past_diff_feat, fut_diff_feat, pres_orig_feat, time_feat, data_roll_mean, data_roll_std]:
-        print(data.shape)
-    data=np.c_[past_diff_feat, fut_diff_feat, pres_orig_feat, time_feat, data_roll_mean, data_roll_std]
+    data=np.c_[past_diff_feat, fut_diff_feat, pres_orig_feat, time_feat, data_roll_mean, data_roll_trend, data_roll_std, data_local_mean, data_local_trend, data_local_std]
 
     print('data shape: %s' % repr(data.shape))
 
