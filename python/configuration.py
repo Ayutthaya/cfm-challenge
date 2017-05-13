759add7bef010279d2075475bb7e117465ca91b9
Nathanaël Foy <nathanael.foy@gmail.com>
2017-05-13 19:59:27 +0200

    increase num pred cv for slower

--- python/configuration.py
+++ python/configuration.py
@@ -50,6 +50,6 @@ elif CONFIGSTRING == 'slower':
     params['nthread'] = 4
     params['eval_metric'] = 'error'
     params['lambda'] = 0.1
-    num_boost_round_cv = 600
+    num_boost_round_cv = 1200
     num_boost_round_pred = 600
 
