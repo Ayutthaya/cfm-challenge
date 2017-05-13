b87dfa826eb3555a6c41d90087f583553a12c0a8
Nathanaël Foy <nathanael.foy@gmail.com>
2017-05-13 15:41:43 +0200

    fix predict_proba

--- python/feature_engineering_cv.py
+++ python/feature_engineering_cv.py
@@ -33,15 +33,15 @@ for stage in ('train', 'test'):
 
     features = {}
 
-    #X = rolling_X(get_data(data, 'nb_trade', 0), -50, 50)
+    X = rolling_X(get_data(data, 'nb_trade', 0), -50, 50)
 
-    #if stage == 'train':
-        #clf_nb_trade.fit(X, label)
+    if stage == 'train':
+        clf_nb_trade.fit(X, label)
 
-    #features['nb_trade_logreg'] = clf_nb_trade.predict(X)
+    features['nb_trade_logreg'] = clf_nb_trade.predict_proba(X)
 
-    tse = two_sided_ema(data)
-    features['two_sided_ema'] = tse
+    #tse = two_sided_ema(data)
+    #features['two_sided_ema'] = tse
 
     #features['3_days_two_sided_ema'] = tse + 0.1 * (day_shift(tse, 1) + day_shift(tse, -1))
 
