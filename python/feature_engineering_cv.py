56d85be8f84a881ec9e44ffd215b10b39385b765
Nathanaël Foy <nathanael.foy@gmail.com>
2017-05-13 16:04:56 +0200

    try bigger window

--- python/feature_engineering_cv.py
+++ python/feature_engineering_cv.py
@@ -33,7 +33,7 @@ for stage in ('train', 'test'):
 
     features = {}
 
-    X = rolling_X(get_data(data, 'nb_trade', 0), -15, 15)
+    X = rolling_X(get_data(data, 'nb_trade', 0), -200, 200)
 
     if stage == 'train':
         clf_nb_trade.fit(X, label)
