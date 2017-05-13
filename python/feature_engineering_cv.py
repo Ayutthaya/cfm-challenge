21a9530a7dd9d5ee15d2bd66e97666445fbbe857
Nathanaël Foy <nathanael.foy@gmail.com>
2017-05-13 17:01:27 +0200

    weight emp

--- python/feature_engineering_cv.py
+++ python/feature_engineering_cv.py
@@ -78,22 +78,22 @@ for stage in ('train', 'test'):
 
     features['bid_right_trend_5'] = np.abs(get_data(data, 'bid_1', 0) - get_rolling(data, 'bid_1', 0, 5).mean())
 
-    features['emp'] = get_data(data, 'bid_size_2', 0) + get_data(data, 'bid_size_1', 0) + get_data(data, 'ask_size_1', 0) + get_data(data, 'ask_size_2', 0)
-    features['entry_emp'] = get_data(data, 'bid_entry_2', 0) + get_data(data, 'bid_entry_1', 0) + get_data(data, 'ask_entry_1', 0) + get_data(data, 'ask_entry_2', 0)
+    features['emp'] = get_data(data, 'bid_size_2', 0) + 0.5*get_data(data, 'bid_size_1', 0) + 0.5*get_data(data, 'ask_size_1', 0) + get_data(data, 'ask_size_2', 0)
+    features['entry_emp'] = get_data(data, 'bid_entry_2', 0) + 0.5*get_data(data, 'bid_entry_1', 0) + 0.5*get_data(data, 'ask_entry_1', 0) + get_data(data, 'ask_entry_2', 0)
 
     X_emp = np.vstack([get_data(data, 'bid_size_2', 0).values, get_data(data, 'bid_size_1', 0).values, get_data(data, 'ask_size_1', 0).values, get_data(data, 'ask_size_2', 0).values]).T
 
-    if stage == 'train':
-        clf_emp.fit(X_emp, label)
+    #if stage == 'train':
+        #clf_emp.fit(X_emp, label)
 
-    features['trained_emp'] = clf_emp.predict_proba(X_emp)[:, 1]
+    #features['trained_emp'] = clf_emp.predict_proba(X_emp)[:, 1]
 
-    X_entry_emp = np.vstack([get_data(data, 'bid_entry_2', 0).values, get_data(data, 'bid_entry_1', 0).values, get_data(data, 'ask_entry_1', 0).values, get_data(data, 'ask_entry_2', 0).values]).T
+    #X_entry_emp = np.vstack([get_data(data, 'bid_entry_2', 0).values, get_data(data, 'bid_entry_1', 0).values, get_data(data, 'ask_entry_1', 0).values, get_data(data, 'ask_entry_2', 0).values]).T
 
-    if stage == 'train':
-        clf_entry_emp.fit(X_entry_emp, label)
+    #if stage == 'train':
+        #clf_entry_emp.fit(X_entry_emp, label)
 
-    features['trained_entry_emp'] = clf_entry_emp.predict_proba(X_entry_emp)[:, 1]
+    #features['trained_entry_emp'] = clf_entry_emp.predict_proba(X_entry_emp)[:, 1]
 
     columnlist = []
     namelist = []
