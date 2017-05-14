aa2621c9f46435e2f77bd3cb5788c3aa8acebe94
Nathanaël Foy <nathanael.foy@gmail.com>
2017-05-14 14:20:51 +0200

    next next before before

--- python/feature_engineering.py
+++ python/feature_engineering.py
@@ -42,10 +42,12 @@ for stage in ('train', 'test'):
     features['two_sided_ema'] = tse
     features['two_sided_ema_day_before'] = day_shift(tse, 1)
     features['two_sided_ema_day_next'] = day_shift(tse, -1)
+    features['two_sided_ema_day_before_before'] = day_shift(tse, 2)
+    features['two_sided_ema_day_next_next'] = day_shift(tse, -2)
 
     nb_trade = get_data(data, 'nb_trade', 0)
     length = nb_trade.shape[0]
-    features['day_mean'] = nb_trade[:(length // 4679)*4679].reshape(4679, -1).mean(axis=1).repeat(length // 4679 + 1)[:length]
+    #features['day_mean'] = nb_trade[:(length // 4679)*4679].reshape(4679, -1).mean(axis=1).repeat(length // 4679 + 1)[:length]
 
     #features['3_days_two_sided_ema'] = tse + 0.1 * (day_shift(tse, 1) + day_shift(tse, -1))
 
